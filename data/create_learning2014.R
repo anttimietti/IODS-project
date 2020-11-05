d85f4944c7cd0b1f6b70bdb44721cda188a7ff79
Antti Miettinen <antti.aj.miettinen@helsinki.fi>
2020-11-05 23:08:26 +0200

    Homework 2 done

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,15 +1,15 @@
 #Name: Antti Miettinen
-#Date: 
-date()
-#File description: ... (one sentence) 
-#Data source: ...
+#Date: 5/11/2011
+#or like this: date()
+#File description: R script for the data wrangling part of Exercise 2
+#Data source: http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt
 
 ##### Data wrangling
 
-# 1) Create a folder named ‘data’ in your IODS-project folder.
+## 1) Create a folder named ‘data’ in your IODS-project folder.
 # Done.
 
-# 2) Read the full learning2014 data into R
+## 2) Read the full learning2014 data into R
 learning2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", 
                 header=TRUE, sep="\t")
 
@@ -25,7 +25,7 @@ dim(learning2014)
 # The output looks like this:
 # [1] 183  60
 
-# 3) Create an analysis dataset with the variables 
+## 3) Create an analysis dataset with the variables 
 
 # gender, age, attitude, deep, stra, surf and points 
 
@@ -33,21 +33,22 @@ dim(learning2014)
 # and also on the bottom part of the following page
 # http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS2-meta.txt. 
  
-
-# Accessing the dplyr library for this
+# Accessing the dplyr library
 library(dplyr)
 
 # Combining questions related to deep, surface and strategic learning
-deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
-surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06", 
+                    "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08",
+                       "SU16","SU24","SU32")
 strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
 
 # Scale all combination variables to the original scales (by taking the mean). 
 
 learning2014$Attitude <- learning2014$Attitude / 10
-#This is done because the column Attitude is a sum of 10 questions related to students 
-#attitude towards statistics, each measured on the Likert scale (1-5). 
-#Scaling the combination variable back to the 1-5 scale.
+# Doing this because the column Attitude is a sum of 10 questions related to students 
+# attitude towards statistics, each measured on the Likert scale (1-5). 
+# Scaling the combination variable back to the 1-5 scale.
 
 # Selecting the columns related to deep learning and creating column 'deep' by averaging
 deep_columns <- select(learning2014, one_of(deep_questions))
@@ -61,10 +62,10 @@ learning2014$surf <- rowMeans(surface_columns)
 strategic_columns <- select(learning2014, one_of(strategic_questions))
 learning2014$stra <- rowMeans(strategic_columns)
 
-
+# Checking that these new columns have appeared
 str(learning2014)
 
-# Selecting only these 7 columns:
+# Selecting only the following 7 columns:
 # gender, age, attitude, deep, stra, surf, points 
 
 keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
@@ -73,27 +74,16 @@ keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
 # Calling the new dataset "learning2014_chosen"
 learning2014_chosen <- select(learning2014, one_of(keep_columns))
 
-
 # print out the column names of the data
 colnames(learning2014_chosen)
 
-# change the name of the second column
+# changing the names of the columns with a capital first letter
 colnames(learning2014_chosen)[2] <- "age"
-
 colnames(learning2014_chosen)[3] <- "attitude"
-
-# change the name of "Points" to "points"
 colnames(learning2014_chosen)[7] <- "points"
 
 # print out the new column names of the data
 colnames(learning2014_chosen)
-
-
-
-###################head(learning2014$Attitude)
-
-
-
 
 # Exclude observations where the exam points variable is zero. 
 
@@ -103,9 +93,7 @@ learning2014_chosen <- filter(learning2014_chosen, points > 0)
 # (The data should then have 166 observations and 7 variables) (1 point)
 # It does.
 
-
-
-# 4) Set the working directory of your R session the iods project folder. 
+## 4) Set the working directory of your R session the iods project folder. 
 # Save the analysis dataset to the ‘data’ folder, using for example write.csv() 
 # or write.table() functions. You can name the data set for example as 
 # learning2014(.txt or .csv). See ?write.csv for help or search the web for 
@@ -113,10 +101,27 @@ learning2014_chosen <- filter(learning2014_chosen, points > 0)
 # by using read.table() or read.csv(). 
 # (Use `str()` and `head()` to make sure that the structure of the data is correct). (3 points)
 
+# Doing the tasks mentioned above:
+
 setwd("~/IODS-project")
 
-?write.csv
+#?write.csv
 
+write.table(learning2014_chosen, file = "data/learning2014_chosen.txt", quote = FALSE, 
+          sep = "\t", col.names = TRUE)
 
+learning2014_test <- read.table("data/learning2014_chosen.txt", sep = "\t")
 
-learningdata <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/learning2014.txt", header=TRUE, sep=",")
+str(learning2014_test)
+head(learning2014_test)
+
+###### TESTING BELOW (not compulsory for the exercise)
+
+# Checking that the wrangled data is similar to the ready dataset on the web 
+
+learningdata_web <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/learning2014.txt", header=TRUE, sep=",")
+
+str(learningdata_web)
+head(learningdata_web)
+
+# It is.
